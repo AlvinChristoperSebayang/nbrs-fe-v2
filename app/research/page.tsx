886f7240c220a4063356f0429e5f6f474bb507d7
SamsulAripin77 <contact.samsularipin@gmail.com>
2026-08-19 10:17:06 +0700

    feat : fixing research add field heading

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -41,7 +41,13 @@ export default async function ResearchPage() {
       />
       <section className="bg-white py-16 lg:py-24">
         <Container className="flex flex-col gap-16 lg:gap-24">
-          <ResearchSection heading={listing?.pageHeading} subheading={listing?.pageSubheading} sectors={listing?.sectors} practices={listing?.practices} items={listing?.articles} />
+          <ResearchSection
+            heading={listing?.researchGridHeading}
+            subheading={listing?.researchGridDescription}
+            sectors={listing?.sectors}
+            practices={listing?.practices}
+            items={listing?.articles}
+          />
           <TextResearchGrid items={listing?.secondaryResearch} />
         </Container>
       </section>
