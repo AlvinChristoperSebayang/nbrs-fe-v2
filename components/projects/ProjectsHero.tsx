af0303e9a4481abf8164eadf9698a972bfccec1d
SamsulAripin77 <contact.samsularipin@gmail.com>
2026-09-01 16:57:57 +0700

    fix : research & project hero lg object top

--- components/projects/ProjectsHero.tsx
+++ components/projects/ProjectsHero.tsx
@@ -20,7 +20,7 @@ export function ProjectsHero({
           src={image || "/images/hero/hero3.png"}
           alt={headingText}
           title={headingText}
-          className="h-full w-full object-cover 2xl:object-top"
+          className="h-full w-full object-cover lg:object-top"
           priority
         />
         <div className="absolute inset-0 bg-black/40" />
